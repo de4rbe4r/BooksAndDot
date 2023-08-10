0c333f9b7b0979c13c90bdcb2e0ea2350fa947c1
KatyaADemina <deminaekaterina01@gmail.com>
2023-08-10 19:38:19 +0400

    Comment...

---
+++ BooksAndDot/BooksAndDot/Models/Orders/Shop.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BooksAndDot.Models.Orders
+{
+    public class Shop
+    { public int Id { get; set; }
+        public  string Title { get; set; }
+        public string Address { get; set; }
+    }
+}
