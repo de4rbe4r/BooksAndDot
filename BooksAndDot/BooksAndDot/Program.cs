58a68acdd05322bb320a8c1186e0f697bee08be4
promiko <itshag63@yandex.ru>
2023-08-10 19:28:27 +0400

    Update UML diagramm database

--- BooksAndDot/BooksAndDot/Program.cs
+++ BooksAndDot/BooksAndDot/Program.cs
@@ -17,10 +17,7 @@ namespace BooksAndDot {
                 db.Database.EnsureCreated();
             }
             CreateHostBuilder(args).Build().Run();
-
         }
-
-
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
