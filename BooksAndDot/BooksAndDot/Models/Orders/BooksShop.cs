2fd2620371efd1ee0a10b6e4fccb9e12638399cd
de4rbe4r <artemdeyev@gmail.com>
2023-08-10 19:45:23 +0400

    Update class BooksShop

--- BooksAndDot/BooksAndDot/Models/Orders/BooksShop.cs
+++ BooksAndDot/BooksAndDot/Models/Orders/BooksShop.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BooksAndDot.Models.Books;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,11 @@ namespace BooksAndDot.Models.Orders
 {
     public class BooksShop
     {
+        public int Id { get; set; }
+        public int ShopId { get; set; }
+        public int BookId { get; set; }
+        public int CountBook { get; set; }
+        public Book Book { get; set; }
+        public Shop Shop { get; set; }
     }
 }
