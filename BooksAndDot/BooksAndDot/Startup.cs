23c20cc06518ef2eb07fcf962069501df2649a8f
de4rbe4r <artemdeyev@gmail.com>
2023-09-07 21:02:20 +0400

    Add swagger authorization

--- BooksAndDot/BooksAndDot/Startup.cs
+++ BooksAndDot/BooksAndDot/Startup.cs
@@ -43,7 +43,28 @@ namespace BooksAndDot {
             services.AddControllers();
             services.AddDbContext<AppDbContext>();
             services.AddSwaggerGen(c => { 
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "BooksAndDot", Version = "v1" }); });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "BooksAndDot", Version = "v1" });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 1safsfsdfdfd\"",
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+        {
+            new OpenApiSecurityScheme {
+                Reference = new OpenApiReference {
+                    Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+            });
             
             // Добавление сервиса аутенфикации. Аутенфикация - процесс проверки что пользователь
             // является тем, за кого себя выдает (проверка по имени и паролю)
