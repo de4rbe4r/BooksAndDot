8373fb6c2fb51ba707b8fca79f99e74fe6706f55
KadoItsuka <delta571@yandex.ru>
2023-09-12 21:11:01 +0400

    Create categories form

---
+++ booksfront/src/components/Categories.js
@@ -0,0 +1,36 @@
+﻿import React, { Component } from 'react'
+
+export class Categories extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            Categories: [
+                {
+                    key: 'all',
+                    name: 'Всё'
+                }
+                {
+                    key: 'Authors',
+                    name: 'Авторы'
+                }
+                 {
+                    key: 'Books',
+                    name: 'Книги'
+                }
+                
+            ]
+        }
+    }
+    render() {
+        return (
+            <div className='categories'>
+                {this.state.Categories.map(el =>(
+                    <div> key={el.key}>{el.name}</div>
+                ))}
+            </div>
+        )
+    }
+}
+
+export default Categories 
+
