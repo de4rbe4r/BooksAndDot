8d7f52e381ced625439dfcaf5cb05c462ee2c4d6
Evgeny BesPredel63 <buyakov@hotmail.com>
2023-09-26 14:02:51 +0400

    Форма авторизации подготовлена к отправке данных на сервер

--- booksfront/src/components/Auth/Authorization.jsx
+++ booksfront/src/components/Auth/Authorization.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import '../../styles/component/auth.css'
 import {useNavigate} from "react-router-dom";
 
 const Authorization = () => {
+
+    const [user, setUser] = useState({
+        email: '',
+        pass: ''
+    })
+
+    const handleAuthUser = (event) => {
+        event.preventDefault()
+        let authUser = {
+            ...user
+        }
+        console.log('Auth: ', authUser)
+    }
 
     // отмена авторизации - редирект на главную страницу
     let navigate = useNavigate()
@@ -17,15 +30,29 @@ const Authorization = () => {
             <form>
                 <h1 className="h3 mb-3 fw-normal text-center">Авторизуйтесь</h1>
                 <div className="form-floating">
-                    <input type="email" className="form-control mb-2" id="floatingInput" placeholder="name@example.com" />
+                    <input
+                        type="email"
+                        className="form-control mb-2"
+                        id="floatingInput"
+                        placeholder="name@example.com"
+                        value={user.email}
+                        onChange={event => setUser({...user, email: event.target.value})}
+                    />
                         <label htmlFor="floatingInput">Введите электронную почту</label>
                 </div>
                 <div className="form-floating">
-                    <input type="password" className="form-control mb-2" id="floatingPassword" placeholder="Password" />
+                    <input
+                        type="password"
+                        className="form-control mb-2"
+                        id="floatingPassword"
+                        placeholder="Password"
+                        value={user.pass}
+                        onChange={event => setUser({...user, pass: event.target.value})}
+                    />
                         <label htmlFor="floatingPassword">Введите пароль</label>
                 </div>
                 <div className='form-btn'>
-                    <button className="btn btn-outline-dark py-2 form-btn-inner" type="submit">Войти</button>
+                    <button className="btn btn-outline-dark py-2 form-btn-inner" type="submit" onClick={handleAuthUser}>Войти</button>
                     <button className="btn btn-outline-danger py-2 form-btn-inner" type="submit" onClick={routeChange}>Отмена</button>
                 </div>
             </form>
