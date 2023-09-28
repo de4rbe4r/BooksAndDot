a0be5775412428b377aa5c1e55ad6043b2b56926
Evgeny BesPredel63 <buyakov@hotmail.com>
2023-09-28 14:51:07 +0400

    Настройка стилей для корректного отображения карточек книг

--- booksfront/src/components/Books/BooksCardList.jsx
+++ booksfront/src/components/Books/BooksCardList.jsx
@@ -16,29 +16,29 @@ const BooksCardList = () => {
     }
 
     return (
-            <div className="col-md-6">
+        <div className="row mb-2">
             {
                 books.map(b => (
-
+                    <div className="col-md-6">
                         <div className="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
                             <div className="col p-4 d-flex flex-column position-static">
                                 <strong className="d-inline-block mb-2 text-primary-emphasis">{b.categories.map(c => (c.title))}</strong>
                                 <h3 className="mb-0">{b.title}</h3>
                                 <div className="mb-1 text-body-secondary">Автор: {b.authors.map(a => (`${a.firstName} ${a.lastName}`)) }</div>
-                                <p className="card-text mb-auto">Знаментый роман великого русского писателя Льва Толстого.</p>
+                                <p className="card-text mb-auto">Описание: </p>
                                 <p className="card-text mb-auto">Год издания: {b.yearPublish}</p>
                                 <a href="#" className="icon-link gap-1 icon-link-hover stretched-link">
                                     Читать далее
                                 </a>
                             </div>
                             <div className="col-auto d-none d-lg-block">
-                                <img className='books-img' src={'http://localhost:5000/' + b.covers.map(cov => (cov.path))} />
+                                <img className='bd-placeholder-img books-img' src={'http://localhost:5000/' + b.covers.map(cov => (cov.path))} />
                             </div>
                         </div>
-
+                    </div>
                 ))
             }
-            </div>
+        </div>
     );
 };
 
